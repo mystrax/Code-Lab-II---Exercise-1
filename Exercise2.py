ac2587bc598cbfde772c68cdded03da14bf5ac76
Jeshua <91404681+mystrax@users.noreply.github.com>
2022-09-22 16:09:23 +0400

    Add files via upload

---
+++ Exercise2.py
@@ -0,0 +1,6 @@
+from math import pi
+area = input("Input the radius:     ")
+result = pi*int(area)*int(area)
+result2 = pi*int(area)*2
+print("This is the area of the circle:  ", result)
+print("Circumference: ", result2)
