0240e9a40d380a29212c463afa82ca2cfba053d5
Jeshua <91404681+mystrax@users.noreply.github.com>
2022-09-22 16:18:36 +0400

    Add files via upload

---
+++ Exercise 3.py
@@ -0,0 +1,4 @@
+name=input("Type your name:   ")
+age=input("Type your age:   ")
+hometown=input("Where is your hometown:    ")
+print("Your Name is:",name,"\nYour Age is:",age,"\nYour Hometown is:",hometown)
