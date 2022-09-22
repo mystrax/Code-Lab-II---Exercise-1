1a52393d4a9a0f24b11d3a1c14ef5d78c2fe4fba
Jeshua <91404681+mystrax@users.noreply.github.com>
2022-09-22 17:42:33 +0400

    Add files via upload

---
+++ Exercise 6.py
@@ -0,0 +1,7 @@
+n=input("Type the number to know if its odd or even:")
+if int(n) % 2 == 0:
+    print(n,"is even")
+elif int(n) % 1==0:
+    print(n,"is odd")
+else:
+    print("Invalid try again")
